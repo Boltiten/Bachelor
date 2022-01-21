01e1f494439369b8f0a0d2b0a6824f8a50e453f5
Boltiten <Morten.Stavik.Eggen@gmail.com>
2022-01-21 12:40:36 +0100

    removed some unessessary

--- filedownloader/rssreader.py
+++ filedownloader/rssreader.py
@@ -10,15 +10,13 @@ url = requests.get(xml)
 soup = BeautifulSoup(url.content, 'xml')
 
 entries = soup.find_all('item') #may also be named 'content' 'item' 'entry' 
-#i = 0
 
 for entry in entries:
     title = entry.title.text
     #summary = entry.summary.text
     link = entry.enclosure['url'] #may be named 'link' 'enclosure'
-    if not os.path.isfile(downloadLocation + title + '.wav'): #str(i)||title
-        download_file(link, downloadLocation + title + '.wav') #str(i)||title
+    if not os.path.isfile(downloadLocation + title + '.wav'):
+        download_file(link, downloadLocation + title + '.wav')
         print(f"Title: {title}\n\nLink:{link}\n\n_______________________________________\n\n")
     else:
         print(f"Title:{title} Already Exist")
-    #i = i +1
