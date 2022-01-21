e44f363e271c0912075d662b3c7bdb3b44663385
Boltiten <Morten.Stavik.Eggen@gmail.com>
2022-01-21 11:32:56 +0100

    Rss reader ready

--- filedownloader/rssreader.py
+++ filedownloader/rssreader.py
@@ -1,8 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+import sys
+import os
 from filedownloader import download_file
 
-url = requests.get('https://rss.art19.com/apology-line')
+xml = sys.argv[1] #First argument is link to rss feed
+downloadLocation = sys.argv[2] #'C:\\Users\\Morten\\Desktop\\School\\Bachelor\\filedownloader\\downloaded\\'
+url = requests.get(xml)
 soup = BeautifulSoup(url.content, 'xml')
 
 entries = soup.find_all('item')
@@ -11,8 +15,11 @@ i = 0
 for entry in entries:
     title = entry.title.text
     #summary = entry.summary.text
-    link = entry.enclosure['url']   
-    download_file(link, 'C:\\Users\\Morten\\Desktop\\School\\Bachelor\\filedownloader\\downloaded\\' + str(i) + '.mp3')
+    link = entry.enclosure['url']
+    if not os.path.isfile(downloadLocation + str(i) + '.wav'):
+        download_file(link, downloadLocation + str(i) + '.wav')
+        print(f"Title: {title}\n\nLink:{link}\n\n_______________________________________\n\n")
+    else:
+        print(f"Title:{title} Already Exist")
     i = i +1
-    print(f"Title: {title}\n\nLink:{link}\n\n_______________________________________\n\n")
 
