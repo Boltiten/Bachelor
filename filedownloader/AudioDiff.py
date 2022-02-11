fa39a5a136535da7d77058882c047cd755590582
Thomas Christ Huru <55877178+ThomasHuru@users.noreply.github.com>
2022-02-11 13:05:45 +0100

    Update AudioDiff.py

--- filedownloader/AudioDiff.py
+++ filedownloader/AudioDiff.py
@@ -2,12 +2,11 @@ import pathlib
 import numpy as np
 import sys
 import csv
-#TODO write code that does this automatically for all folders
 #TODO unit tests
 noAdLoc = sys.argv[1]
 adLoc = sys.argv[2]
 #Example command for running the program
-# python AudioDiff.py "C:/Users/Thomas/Downloads/-tor-The EPA's Lies About Air Pollution Are Killing Americans.mp3" "C:/Users/Thomas/Downloads/-nor-The EPA's Lies About Air Pollution Are Killing Americans.mp3" 5000
+# python AudioDiff.py "-tor-The EPA's Lies About Air Pollution Are Killing Americans.mp3" "-nor-The EPA's Lies About Air Pollution Are Killing Americans.mp3" 5000
 # Note that the last sysarg is for bytelenght of segemntations, and you need ad + noad folder ready before running
 noAdFrames = np.fromfile(noAdLoc, dtype='uint8')
 adFrames = np.fromfile(adLoc, dtype='uint8')
@@ -30,15 +29,18 @@ def main():
     i=0
     y=0
     breakPointArr = []
-    breakPointArr.append(getFileName(sys.argv[1]))
-    while i<len(noAdFrames):
+    breakPointArr.append(getFileName(sys.argv[2],"/"))
+    print(breakPointArr[0])
+    finished = False
+    while i<len(noAdFrames) or not finished:
         while((noAdFrames[i]==adFrames[y]) and i<len(noAdFrames) ):
             if(i+2>len(noAdFrames)):
                 print("finished")
+                finished = True
                 break
             i=i+1
             y=y+1
-        ret=findMatchWindow(10,noAdFrames,adFrames,i,y)
+        ret=findMatchWindow(100,noAdFrames,adFrames,i,y)
         if(not ret[1]):
             break
         breakPointArr.append(y)
@@ -46,12 +48,12 @@ def main():
         y=ret[0]
         print(i," ",y)
     csvFile = open('adBreakpoints.csv','a', newline="")
-    fileString = getFileName(adLoc)
-    csvWriter = csv.writer(csvFile)
+    fileString = getFileName(adLoc,",")
+    csvWriter = csv.writer(csvFile, delimiter="|")
     csvWriter.writerow(breakPointArr)
     csvFile.close()
     prevSegLen = 0
-    for i in range(1,(int)(len(breakPointArr))):
+    for i in range(1,(int)(len(breakPointArr))-1):
         prevSegLen = splitAudio(int(sys.argv[3]),breakPointArr[i],breakPointArr[i+1],fileString,i,prevSegLen)
     print("done")
 
@@ -75,9 +77,9 @@ def splitAudio(segmentBitLength,startpos,endpos,filenameString,oddMeansAd,prevNu
     return(fileCount-fileBefore)
 
 #For generating unique name for podcasts
-def getFileName(filePath):
+def getFileName(filePath,delimeter):
     path = pathlib.PurePath(filePath)
-    return path.parent.parent.name+ "," + path.parent.name +","+ path.name
+    return path.parent.name +delimeter+ path.name
 
 if __name__ == "__main__":
     main()
