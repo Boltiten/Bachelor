d07744e8e136f9ef97b416b2ae2e2076c3a9237d
Thomas Christ Huru <55877178+ThomasHuru@users.noreply.github.com>
2022-03-28 13:33:58 +0200

    Update ConvertToWav.py

--- podcasts/ConvertToWav.py
+++ podcasts/ConvertToWav.py
@@ -5,7 +5,7 @@ rootdir = Path("C:\\Users\\Thomas\\Downloads\\podcasts")
 file_list = [f for f in rootdir.resolve().glob('**/*') if f.is_file()]
 for file in file_list:
     name, ext = file.name.rsplit(".",1)
-    if ext == "mp3":
+    if ext == "mp3" and (file.parent.name=="ad" or file.parent.name == "noad"):
         if name[0] == "-":
             print(file.parent.name)
             file.rename(file.parent.name+ "/"+ file.name.replace("-","",1))
