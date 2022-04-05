63c56b478284ff20cc0129996d113ee3f10bb7fe
Thomas Christ Huru <55877178+ThomasHuru@users.noreply.github.com>
2022-04-05 14:51:08 +0200

    Update massAudioDif.py

--- scripts/massAudioDif.py
+++ scripts/massAudioDif.py
@@ -1,7 +1,9 @@
 import os
 import pandas as pd
+import os
+import pandas as pd
 LANGUAGES = ["nor","eng","de"]
-LOCATION = "E:/Podcasts/"
+LOCATION = "E:/Podcasts"
 try:
     data = pd.read_csv('adBreakpoints.csv',header=None,encoding='cp1252',sep="|").fillna
     data = data[0].str.split('|')[0]
@@ -9,12 +11,8 @@ try:
     csvList = csvList.values.tolist()
 except:
     csvList=[]
-os.chdir(os.getcwd())
-os.chdir("..")
-os.chdir("podcasts")
-for path, subdir, files in os.walk(os.getcwd()):
+for path, subdir, files in os.walk(LOCATION):
     for name in files:
-        print(name)
         fullName = os.path.join(path,name)
         fullName = fullName.replace("\\","/")
         found = False
