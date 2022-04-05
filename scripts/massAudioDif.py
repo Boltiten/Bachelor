fbb3f5ae5dc44cb519b093336a2d450128a91aa8
Thomas Christ Huru <55877178+ThomasHuru@users.noreply.github.com>
2022-04-05 15:10:10 +0200

    Update massAudioDif.py

--- scripts/massAudioDif.py
+++ scripts/massAudioDif.py
@@ -1,7 +1,14 @@
+from dataclasses import replace
 import os
 import pandas as pd
 LANGUAGES = ["nor","eng","de"]
 LOCATION = "E:/Podcasts"
+
+currentfile=os.getcwd()
+
+currentfile = currentfile.replace("\\","/")
+audioDifLoc = currentfile.replace("/scripts","/podcasts")
+
 try:
     data = pd.read_csv('adBreakpoints.csv',header=None,encoding='cp1252',sep="|").fillna
     data = data[0].str.split('|')[0]
@@ -26,7 +33,6 @@ for path, subdir, files in os.walk(LOCATION):
             if (not fullName in csvList):
                 Tor = fullName.replace("-"+foundLang+"-","-tor-").replace(foundLangslash,"tor/")
                 Tor = Tor.replace("-"+foundLang+"-","-tor-")
-                print('python AudioDiff.py "' + Tor +'" "'+ fullName+'"')
-                os.system('python AudioDiff.py "' + Tor +'" "'+ fullName+'"')
-
+                print('python '+audioDifLoc+ '/AudioDiff.py "' + Tor +'" "'+ fullName+'"')
+                os.system('python '+audioDifLoc+ '/AudioDiff.py "' + Tor +'" "'+ fullName+'"')
     
