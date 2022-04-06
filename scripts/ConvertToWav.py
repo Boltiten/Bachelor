0aedbdd044c21e121db58a0987531a940deab84c
Thomas Christ Huru <55877178+ThomasHuru@users.noreply.github.com>
2022-04-06 10:13:40 +0200

    Update ConvertToWav.py

--- scripts/ConvertToWav.py
+++ scripts/ConvertToWav.py
@@ -1,11 +1,14 @@
-from pathlib import Path
+from pathlib import Path, PurePath
 import subprocess
-rootdir = Path(__file__).parent.parent.absolute() / "podcasts"
+LOCATION="D:/Podcasts"
+rootdir=Path(LOCATION)
+#rootdir = Path(__file__).parent.parent.absolute() / "podcasts"
 file_list = [f for f in rootdir.resolve().glob('**/*') if f.is_file()]
 for file in file_list:
     name, ext = file.name.rsplit(".",1)
     if ext == "mp3" and (file.parent.name=="ad" or file.parent.name == "noad"):
-        if name[0] == "-":
-            print(file.parent.name)
-            file.rename(file.parent.name+ "/"+ file.name.replace("-","",1))
-        subprocess.call(['ffmpeg', '-i',name+".mp3", name.replace(" ","")+".wav"])
+        #if name[0] == "-":
+            #print(file.parent.name)
+            #file.rename(file.parent.name+ "/"+ file.name.replace("-","",1))
+        print(LOCATION+"/"+file.parent.name+"/"+name.replace(" ","")+".wav")
+        subprocess.call(['ffmpeg', '-i',file.resolve(), LOCATION+"/"+file.parent.name+"/"+name.replace(" ","")+".wav"])
