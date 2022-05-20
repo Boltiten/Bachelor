434c68291b0e9d7862325b7f88e37f28ce571719
Thomas Christ Huru <55877178+ThomasHuru@users.noreply.github.com>
2022-05-20 07:58:14 +0200

    use sys.argv for audioDif and cleaning

--- scripts/massAudioDif.py
+++ scripts/massAudioDif.py
@@ -1,12 +1,16 @@
 import os
 import pandas as pd
+import sys
+#The languages being used, these languages are folder names of downloaded podcasts
 LANGUAGES = ["nor","eng","de"]
-LOCATION = "D:/Podcasts"
+#Folder containing podcasts
+LOCATION=sys.argv[1]
 
 currentfile = os.path.dirname(os.path.abspath(__file__))
 currentfile = currentfile.replace("\\","/")
 audioDifLoc = currentfile.replace("/scripts","/podcasts")
 
+#Read csv to speed up process if stopped halfway
 try:
     data = pd.read_csv('adBreakpoints.csv',header=None,encoding='cp1252',sep="|").fillna
     data = data[0].str.split('|')[0]
@@ -24,15 +28,15 @@ for path, subdir, files in os.walk(LOCATION):
              if lang+"/" in fullName:
                 found = True
                 foundLang = lang
-                foundLangslash = lang+"/"
+                foundLangSlash = lang+"/"
+        #Iterates through language folder if found
         if (found):
-            fullName = foundLangslash + fullName.split(foundLangslash)[1]
+            fullName = foundLangSlash + fullName.split(foundLangSlash)[1]
             fullName = fullName.replace("\\","/")
             if (not fullName in csvList):
-                Tor = fullName.replace("-"+foundLang+"-","-tor-").replace(foundLangslash,"tor/")
+                Tor = fullName.replace("-"+foundLang+"-","-tor-").replace(foundLangSlash,"tor/")
                 Tor = Tor.replace("-"+foundLang+"-","-tor-")
                 Tor = LOCATION+ "/" + Tor
                 fullName = LOCATION+ "/" + fullName
-                print('python3 '+audioDifLoc+ '/AudioDiff.py "' + Tor +'" "'+ fullName+'"')
-                os.system('python3 '+audioDifLoc+ '/AudioDiff.py "' + Tor +'" "'+ fullName+'"')
-    
+                print('python3 '+currentfile+ '/AudioDiff.py "' + Tor +'" "'+ fullName+'" "'+LOCATION+'"')
+                os.system('python3 '+currentfile+ '/AudioDiff.py "' + Tor +'" "'+ fullName+'" "'+LOCATION+'"')
